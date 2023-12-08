105ea86000fd5c6b12f255871364b4087b1684e8
shivam4301 <153324656+shivam4301@users.noreply.github.com>
2023-12-08 15:44:42 -0500

    Delete Project directory

--- Project/footer.js
+++
@@ -1,6 +0,0 @@
-// Fetch and insert the footer content into the footer placeholder
-fetch("./footer.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("footer-placeholder").innerHTML = data;
-  });
--- Project/header.js
+++
@@ -1,6 +0,0 @@
-// Fetch and insert the header content into the header placeholder
-fetch("./header.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("header-placeholder").innerHTML = data;
-  });
